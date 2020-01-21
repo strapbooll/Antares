85042d8517afc9eb859f95098effad61f310a8f4
Rafael RX <rafael_rx@hotmail.com>
2020-01-20 23:03:19 -0300

    Backend com Node.JS salvando fotos local, S3 e MongoDB

---
+++ backend/src/routes.js
@@ -0,0 +1,33 @@
+const routes = require('express').Router();
+const multer = require('multer');
+const multerConfigs = require('./config/multer');
+
+const Post = require('./models/Posts');
+
+routes.get('/posts', async (req, res) => {
+    const posts = await Post.find();
+
+    return res.json(posts);
+});
+
+routes.post('/posts', multer(multerConfigs).single("file"), async (req, res) => {
+    const { originalname: name, size, key, location: url = '' } = req.file;
+    const post = await Post.create({
+        name,
+        size,
+        key,
+        url,
+    });    
+    return res.json(post);
+});
+
+routes.delete('/posts/:id', async (req, res) => {
+    console.log(req.params.id);
+    const post = await Post.findById(req.params.id);
+
+    await post.remove();
+
+    return res.send();
+});
+
+module.exports = routes;
