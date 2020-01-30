c064644d4c7dc7bbb6faa7394fca19e232f71077
Rafael RX <rafael_rx@hotmail.com>
2020-01-29 23:00:31 -0300

    Inclusçao da cors no backend e e integração do frontend com a API

--- frontend/src/components/FileList/index.js
+++ frontend/src/components/FileList/index.js
@@ -4,32 +4,50 @@ import { CircularProgressbar } from "react-circular-progressbar";
 import { Container, FileInfo, Preview } from "./styles";
 import { MdCheckCircle, MdError, MdLink } from "react-icons/md";
 
-const FileList = () => (
+const FileList = ({ files, onDelete }) => (
   <Container>
-    <li>
-      <FileInfo>
-        <Preview src="C:\Users\rafae\OneDrive\Imagens\2015\2015-04-09 11.47.38.jpg" />
+    {files.map(uploadedFile => (
+      <li key={uploadedFile.id}>
+        <FileInfo>
+          <Preview src={uploadedFile.preview} />
+          <div>
+            <strong>{uploadedFile.name}</strong>
+            <span>
+              {uploadedFile.readableSize}{" "}
+              {!!uploadedFile.url && (
+                <button onClick={() => onDelete(uploadedFile.id)}>
+                  Excluir
+                </button>
+              )}
+            </span>
+          </div>
+        </FileInfo>
         <div>
-          <strong>Profile.png</strong>
-          <span>
-            64 kb <button onClick={() => {}}>Excluir</button>
-          </span>
+          {!uploadedFile.uploaded && !uploadedFile.error && (
+            <CircularProgressbar
+              styles={{
+                root: { width: 24 },
+                path: { stroke: "#7159c1" }
+              }}
+              strokeWidth={10}
+              percentage={uploadedFile.progress}
+            />
+          )}
+
+          {uploadedFile.url && (
+            <a
+              href={uploadedFile.url}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <MdLink style={{ marginRight: 8 }} size={24} color="#222" />
+            </a>
+          )}
+          {uploadedFile.uploaded && <MdCheckCircle size={24} color="#78e5d5" />}
+          {uploadedFile.error && <MdError size={24} color="#e57878" />}
         </div>
-        <CircularProgressbar
-          styles={{
-            root: { width: 24 },
-            path: { stroke: "#7159c1" }
-          }}
-          strokeWidth={10}
-          percentage={60}
-        />
-        <a href="#" target="_blank" rel="noopener noreferrer">
-          <MdLink style={{ marginRight: 8 }} size={24} color="#222" />
-        </a>
-        <MdCheckCircle size={24} color="#78e5d5" />
-        <MdError size={24} color="#e57878" />
-      </FileInfo>
-    </li>
+      </li>
+    ))}
   </Container>
 );
 
--- frontend/src/components/FileList/styles.js
+++ frontend/src/components/FileList/styles.js
@@ -35,4 +35,13 @@ export const FileInfo = styled.div`
   }
 `;
 
-export const Preview = styled.div``;
+export const Preview = styled.div`
+  height: 36px;
+  width: 36px;
+  border-radius: 5px;
+  background-image: url(${props => props.src});
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: 50% 50%;
+  margin-right: 10px;
+`;
